2ff49b0cbcff685dd57d4e8108ad3139dd6ae52f
Ayishathesni <ayshathesni001@gmail.com>
2024-10-13 22:30:43 +0530

    templates complete

--- Products/urls.py
+++ Products/urls.py
@@ -1,7 +1,7 @@
 """
 URL configuration for brokart project.
 
-The `urlpatterns` list routes URLs to views. For more information please see:
+The `urlpatterns` lfrom django.contrib import admist routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.1/topics/http/urls/
 Examples:
 Function views
@@ -14,14 +14,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.urls import path,include
-from django.contrib import admin
+
 from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('Products.urls'))
+    path('', views.index),
 ]
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
