c36ee4c191d94115dcbdd2b08fc47cbc5dd5193b
Ayishathesni <ayshathesni001@gmail.com>
2024-10-16 23:04:34 +0530

    layout finish home,products,description,account,cart

--- brokart/urls.py
+++ brokart/urls.py
@@ -22,6 +22,10 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('Products.urls'))
+    path('', include('Products.urls')),
+    path('customer/', include('Customers.urls')),
+    path('Orders/', include('Orders.urls'))
+
+
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
